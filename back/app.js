a0e9e30b1049e63f2575bed552d75bc8f8307607
smilejakdu <ash982416@gmail.com>
2021-01-17 14:37:44 +0900

    back_express

--- back/app.js
+++ back/app.js
@@ -1,8 +1,30 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    res.end('hello world');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello express')
 });
-server.listen(3065, () => {
-    console.log('server running')
+
+app.get('/api', (req, res) => {
+    res.send('hello api');
+})
+
+app.get('/api/posts', (req, res) => {
+    res.json([
+        {id: 1, content: 'hello'},
+        {id: 2, content: 'hello2'},
+        {id: 3, content: 'hello3'},
+    ]);
+});
+
+app.post('/api/posts', (req, res) => {
+    res.json({id: 1, content: 'hello'});
+});
+
+app.delete('/api/post', (req, res) => {
+
+});
+
+app.listen(3065, () => {
+    console.log('server running');
 });
